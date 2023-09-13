0da17b12e57a44f5baddb54e3142883931a8059c
s01k1m <sk618dev@gmail.com>
2023-09-13 18:04:40 +0900

    feat: access token 받아오는 것까지 구현
    Former-commit-id: b5a37c205a80472fab99d9d19306565e491da4c3

---
+++ frontend/app/signup/deposit/page.tsx
@@ -0,0 +1,9 @@
+import Script from "next/script";
+
+export default function CreateDepositAccount() {
+  return (
+    <main>
+      <span>저축계좌</span>
+    </main>
+  );
+}
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `http://25.4.167.82:8080/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
