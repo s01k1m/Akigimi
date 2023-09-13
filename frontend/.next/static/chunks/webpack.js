c95a3aa10ea7df3f4d584d9449cda8b239d1aa75
seoyoung81 <seosue0801@gmail.com>
2023-09-13 18:00:36 +0900

    design: 피드 디자인

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21c4437e51252369"; }
+/******/ 		__webpack_require__.h = function() { return "658b5dd72875e329"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ frontend/components/Feed/FeedItem.tsx
@@ -0,0 +1,11 @@
+import FeedReceipt from "./FeedReceipt"
+
+const FeedItem = () => {
+    return (
+        <div>
+            <FeedReceipt />
+        </div>
+    )
+}
+
+export default FeedItem
