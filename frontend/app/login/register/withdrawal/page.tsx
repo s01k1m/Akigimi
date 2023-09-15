2aa5da3786abe17c8bd8802eaa926963d85c3ab2
s01k1m <sk618dev@gmail.com>
2023-09-15 18:01:10 +0900

    chore: 계좌 비밀번호 로직 진행중
    Former-commit-id: 387889a93255e82d3824e5fba7ab7017945ae710

--- frontend/app/login/register/withdrawal/page.tsx
+++ frontend/app/login/register/withdrawal/page.tsx
@@ -1,9 +1,102 @@
-import styled from "styled-components";
+"use client";
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import { redirect, useRouter } from "next/navigation";
+import { BasicButton } from "@/components/login/BasicButton";
 
-export default function RegisterWithdrawalAccount() {
+export interface RegisterInfo {
+  title: string;
+  menual: string;
+}
+
+export default function Signup() {
+  const [isLoading, setIsLoading] = useState<boolean>(false); // 페이지 로딩 중인지 확인하기 위하여
+  const [password1, setPassword1] = useState<string>("");
+  const [didMatch, setDidMatch] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const router = useRouter();
+  const [content, setContent] = useState<RegisterInfo[] | undefined>([]);
+  const token = window.localStorage.getItem("access_token");
+  const info = {
+    title: "출금계좌를 연결하고 있어요!",
+    menual: "필수 정보를 입력해주세요",
+  };
+
+  // TODO: 비밀번호 암호화
+  // TODO: 비밀번호1 비밀번호2 같은지 확인 로직
+  // TODO: 폼 컴포넌트화등 리팩토링에 대한 고민
+
+  const handleClick = async () => {
+    router.push("/login/register/deposit");
+
+    const formData = new FormData();
+    formData.append("password", password1); // 파일 첨부
+
+    await axios
+      .post("/api/user/nickname", formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
   return (
-    <div>
-      <h1>계정을 만들고 있어요</h1>
+    <div className="h-full w-full">
+      {isLoading ? (
+        <div>로딩중</div>
+      ) : (
+        <div className="p-9 relative h-full">
+          <div className="header text-[22px] pt-2 pb-[14px] font-bold">
+            {info.title}
+          </div>
+          <div className="explanation text-base mb-[65px] text-grey0">
+            {info.menual}
+          </div>
+          <label htmlFor="password1" className="font-semibold">
+            출금계좌 비밀번호
+          </label>
+          <div className="flex justify-around my-3">
+            <input
+              type="password"
+              id="pass"
+              name="password1"
+              required={true}
+              minLength={4}
+              maxLength={4}
+              value={password1}
+              onChange={(e) => {
+                console.log(e.target.value);
+                setPassword1(e.target.value);
+              }}
+              className="bg-[#F2F2F2] h-9 w-68 flex-1 p-2 rounded"
+            ></input>
+          </div>
+
+          <div
+            className="absolute inset-x-0 bottom-0 mb-8"
+            onClick={(e) => {
+              if (!didMatch) {
+                e.preventDefault;
+                alert("출금 비밀번호를 제대로 입력해주세요");
+                return false;
+              }
+            }}
+          >
+            {didMatch ? (
+              <BasicButton
+                msg={"NEXT"}
+                handleClick={() => handleClick()}
+              ></BasicButton>
+            ) : (
+              <BasicButton msg={"NEXT"} handleClick={() => {}}></BasicButton>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
