f0064db406b9383da13876ce4e82bce333fcdd08
s01k1m <sk618dev@gmail.com>
2023-09-15 16:02:14 +0900

    feat: 로그인하기 버튼 스켈레톤 ui
    버튼이 링크를 가져올때까지 스켈레톤 ui가 화면에 뜨도록 함

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import Script from "next/script";
 
 export default function Home() {
   return (
     <main>
       <span>스켈레톤 코드</span>
+      <br></br>
+      <Link href="/login">로그인</Link> <br></br>
+      <Link href="/login/register">닉네임</Link> <br></br>
+      <Link href="/login/register/withdrawal">출금계좌등록</Link> <br></br>
+      <Link href="/login/register/deposit">입금계좌등록</Link> <br></br>
     </main>
   );
 }
