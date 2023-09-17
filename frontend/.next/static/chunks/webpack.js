6959382a33ac68082da77187e26c1e2cb3d0af34
seoyoung81 <seosue0801@gmail.com>
2023-09-17 14:38:57 +0900

    fix: 피드 마진 값 자동으로 반영되게 수정

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb42872ae843d4b7"; }
+/******/ 		__webpack_require__.h = function() { return "a8cd217aea66dc18"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
