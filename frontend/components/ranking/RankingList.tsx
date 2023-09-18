a034a039e11416f851546ccd12434631d480846e
seoyoung81 <seosue0801@gmail.com>
2023-09-18 17:54:42 +0900

    feat: 랭킹 기본 UI
    Former-commit-id: 62455eb94ee37f96ec157af99375c6c6f6fd2525

---
+++ frontend/components/ranking/RankingList.tsx
@@ -0,0 +1,29 @@
+import RankingItem from "./RankingItem"
+const RankingList = () => {
+    const rankingData = [
+        {id: 1, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 80},
+        {id: 2, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 70},
+        {id: 3, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 60},
+        {id: 4, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 80},
+        {id: 5, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 80},
+        {id: 6, imgUrl: '/kakao logo.png', userName: '영혼없는 고서영', product: '나이키 어쩌구 신발', gage: 80},
+    ]
+    return (
+        <div>
+            {rankingData.map((item: any) => (
+                <RankingItem
+                    key={item.id}
+                    id={item.id}
+                    imgUrl={item.imgUrl}
+                    userName={item.userName}
+                    product={item.product}
+                    gage={item.gage}
+                />
+            ))
+
+            }
+        </div>
+    )
+}
+
+export default RankingList
