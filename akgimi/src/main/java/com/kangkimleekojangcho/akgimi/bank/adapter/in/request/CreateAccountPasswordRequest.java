58fda02bab6bbfdaf558cc4a515ee3b8c51fb745
이승민 <0283esm@naver.com>
2023-09-18 14:18:37 +0900

    [S09P22A110-434] feat: 비밀번호 생성 로직 틀 작성
    Former-commit-id: 9fdd2ff7c21549a2ce880a0606e7463ae54c5057

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/adapter/in/request/CreateAccountPasswordRequest.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/adapter/in/request/CreateAccountPasswordRequest.java
@@ -1,11 +1,18 @@
 package com.kangkimleekojangcho.akgimi.bank.adapter.in.request;
 
+import com.kangkimleekojangcho.akgimi.bank.application.request.CreateAccountPasswordServiceRequest;
+import com.kangkimleekojangcho.akgimi.bank.domain.AccountType;
 import com.kangkimleekojangcho.akgimi.bank.domain.Bank;
 import lombok.Getter;
 
 @Getter
 public class CreateAccountPasswordRequest {
     private Bank bank;
+    private AccountType accountType;
     private String accountNumber;
     private String password;
+
+    public CreateAccountPasswordServiceRequest toServiceRequest() {
+        return new CreateAccountPasswordServiceRequest(bank,accountType,accountNumber,password);
+    }
 }
