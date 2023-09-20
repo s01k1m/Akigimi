ae6362ed39fff7f92d73750393bdd1a60e559c0b
KBS <qudtjs3636@gmail.com>
2023-09-20 14:33:59 +0900

    refactor: D FeedController test isplayname 변경
    Signed-off-by: KBS <qudtjs3636@gmail.com>

--- akgimi/src/test/java/com/kangkimleekojangcho/akgimi/ControllerTestSupport.java
+++ akgimi/src/test/java/com/kangkimleekojangcho/akgimi/ControllerTestSupport.java
@@ -1,11 +1,13 @@
 package com.kangkimleekojangcho.akgimi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kangkimleekojangcho.akgimi.common.application.MattermostSender;
 import com.kangkimleekojangcho.akgimi.global.config.WebSecurityConfig;
 import com.kangkimleekojangcho.akgimi.sns.adapter.in.FeedController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.access.SecurityConfig;
@@ -21,6 +23,9 @@ import org.springframework.test.web.servlet.MockMvc;
 )
 @AutoConfigureMockMvc(addFilters = false) // 필터 비활성화
 public abstract class ControllerTestSupport {
+
+    @MockBean
+    protected MattermostSender mattermostSender;
 
     @Autowired
     protected MockMvc mockMvc;
