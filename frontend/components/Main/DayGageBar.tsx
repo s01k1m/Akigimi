08e2fd070ec93a2e5ebd5d889531a9071c6050fb
seoyoung81 <seosue0801@gmail.com>
2023-09-20 21:04:58 +0900

    feat: 날짜 게이지바 달리는 이모지 크기 퍼센트 변해도 크기 변하지 않게 하기
    Former-commit-id: e19ec5e3ee58a995ab3eb289c86da03b573f5ee2

--- frontend/components/Main/DayGageBar.tsx
+++ frontend/components/Main/DayGageBar.tsx
@@ -17,16 +17,16 @@ const DayGageBar: React.FC<DayPropsType> = ({ days, challengePeriod }) => {
         <>
         <div className='Gage-Bar relative flex items-center justify-center'>
             <p className='flex z-10'>
-                {days}/{challengePeriod}
+                {1}/{50}
             </p>
-            <div className='Date-Gage z-0 absolute top-1/2 left-0 transform -translate-y-1/2'  style={{ width: `${(days/challengePeriod)*100}%` }}>
+            <div className='Date-Gage z-0 absolute top-1/2 left-0 transform -translate-y-1/2 min-w-[10%]'  style={{ width: `${(1/50)*100}%` }}>
                 <div className="flex items-center translate-y-2">
                     <Image 
                         src="/images/runner.png" 
                         alt="gift" 
                         width={60} 
                         height={60} 
-                        className="z-0 absolute right-[-25px]"/>
+                        className="z-0 absolute right-[-25px] min-w-[50px]"/>
                 </div>
             </div>
         </div>
