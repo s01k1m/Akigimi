8521b2c3d9d58c9f7963e06fb1c6a70b10e5eca9
zogak7 <zogak7@gmail.com>
2023-09-20 13:11:02 +0900

    [S09P22A110-439] chore: product data 이미지 링크 수정
    Former-commit-id: 63d8be4b7da08f3bb3075b5b28480be7d977b067

--- akgimi/src/main/resources/data.sql
+++ akgimi/src/main/resources/data.sql
@@ -11,9 +11,9 @@ insert into account(id, account_type, is_deleted, is_password_registered, balanc
     values (2, 1, false, true, 50000, now(),now(), 9999, '1231-4515-114', 'MULTI','1234');
 
 insert into product(id, name, thumbnail, image, detail, url, price, is_deleted)
-values (1, '닌텐도DS', '썸네일', '이미지', '디테일', '링크', 20000, 0);
+values (1, '닌텐도DS', '/images/nintendo.png', '/images/nintendo.png', '디테일', '/images/nintendo.png', 20000, 0);
 insert into product(id, name, thumbnail, image, detail, url, price, is_deleted)
-values (2, '시그니엘숙박권', '썸네일', '이미지', '디테일', '링크', 30000, 0);
+values (2, '시그니엘숙박권', '/images/nintendo.png', '/images/nintendo.png', '디테일', '/images/nintendo.png', 30000, 0);
 
 insert into challenge(id, user_id, product_id, accumulated_amount, achievement_state, challenge_period, is_in_progress, achievement_date, created_at, updated_at)
 values (999, 9999, 2, 20000, true, 30, false, '2023-08-08', current_timestamp, current_timestamp);
