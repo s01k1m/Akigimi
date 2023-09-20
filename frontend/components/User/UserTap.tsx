2df4077bb25b70991db8fd25a1ac194fe803936a
s01k1m <sk618dev@gmail.com>
2023-09-20 14:16:28 +0900

    feat: 마이페이지 UI
    1. 내 계좌 조회
    2. 팔로우 팔로잉 조회 UI 했습니다
    
    
    Former-commit-id: 41c681f908de0f4fe1f1e5b194e1e7891a8823b5

---
+++ frontend/components/User/UserTap.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState, useEffect } from "react";
+import "@/styles/Mypage.css";
+import { useRouter } from "next/navigation";
+
+const MypageTap = () => {
+  const [selectedValue, setSelectedValue] = useState<string>("feeds");
+  const router = useRouter();
+  // selectedValue 값을 친구 로
+  useEffect(() => {
+    const defaultSelected = document.getElementById(
+      "value-1"
+    ) as HTMLInputElement;
+    if (defaultSelected) {
+      defaultSelected.checked = true;
+    }
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="radio-input">
+          <label>
+            <input
+              value="feeds"
+              name="value-radio"
+              id="value-1"
+              type="radio"
+              onClick={() => {
+                router.push("/mypage/myfeeds");
+              }}
+              checked={selectedValue === "feeds"}
+              defaultChecked
+            />
+            <span>영수증</span>
+          </label>
+          <label>
+            <input
+              value="goods"
+              name="value-radio"
+              id="value-2"
+              type="radio"
+              onClick={() => {
+                router.push("/mypage/mygoods");
+              }}
+              checked={selectedValue === "goods"}
+            />
+            <span>물건</span>
+          </label>
+          <div className="selection"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MypageTap;
