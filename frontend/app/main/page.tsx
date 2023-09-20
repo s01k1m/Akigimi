0a7c66b4c929e29f46e7184b03687b9805e60add
seoyoung81 <seosue0801@gmail.com>
2023-09-20 13:17:44 +0900

    feat: 챌린지 조회 API 연동
    Former-commit-id: d924d978776a9d3c9450de106939acb108aebe58

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -7,13 +7,75 @@ import CharacterImg from "@/components/Main/CharacterImg"
 import ChallengeInfo from "@/components/Main/ChallengeInfo"
 import GoToBuyBtn from "@/components/Main/GoToBuyBtn"
 import GoToRetryBtn from "@/components/Main/GoToRetryBtn"
-import { useQuery } from "react-query"
 import axios from 'axios'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Main = () => {
 
-    const [stage, setState] = useState<number>(0)
+    const token = `eyJ0eXBlIjoiQUNDRVNTVE9LRU4iLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OTk5OSwidXNlclN0YXRlIjoiUEVORElORyIsImlhdCI6MTY5NTE3NDc3NywiZXhwIjoxNjk1MzU0Nzc3fQ.58F3t3w_nBSCD0wRrwExXc4VTdPJSrGBiqRwjlQ4XjU`
+    const [stage, setStage] = useState<number>(0)
+    const [percentage, setPercentage] = useState<number>(0)
+    const [productName, setProductName] = useState<string>("")
+    const [productPrice, setProductPrice] = useState<number>(0)
+    const [productImg, setProductImg] = useState<string>("")
+    const [challengePeriod, setChallengePeriod] = useState<number>(0)
+    const [days, setDays] = useState<number>(0)
+    
+    const challengeData = async () => {
+        await axios
+            .get('http://25.4.167.82:8080/api/challenges/in-progress', {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            })
+            .then((response) => {
+                console.log('챌린지 조회 성공', response.data)
+                const data = response.data.data
+                console.log('챌린지 상태', response.data.data.characterStatus)
+                // setStage(data.characterStatus + 1)
+                setProductName(data.productName)
+                setProductPrice(data.productPrice)
+                setProductImg(data.productImg)
+                setChallengePeriod(data.challengePeriod)
+                setDays(data.days)
+            })
+            .catch((error) => {
+                console.error('챌린지 조회 에러', error)
+            })
+    } 
+
+    // 하드 데이터 용 api
+    const inProgress = async () => {
+        const requestBody = {
+            itemId: 1,
+            challengePeriod: 50
+        };
+        await axios
+            .post('http://25.4.167.82:8080/api/challenges', requestBody, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response) => {
+                console.log('챌린지 생성 성공', response.data)
+            })
+            .catch((error) => {
+                if (error.response.data.code === '011') {
+                    console.log('자원이 존재하지 않는 경우')
+                } else if (error.response.data.code === '017') {
+                    console.log('이미 챌린지 진행 중인 경우')
+                    alert('이미 챌린지를 진행중입니다')
+                }
+                console.error('챌린지 생성 에러', error)
+            })
+    } 
+
+    useEffect(() => {
+        challengeData()
+        console.log(stage)
+    }, [token])
+
   
     return (
         <div className={`background-${stage}`} style={{ width: '100%'}}>
@@ -23,12 +85,22 @@ const Main = () => {
         </div>
         {stage === 0 &&
             <div className="flex justify-center mt-60" >
-                <GoToSelectItem />  
+                  {/* 물건 정하러 가기 버튼  */}
+                  <button 
+                    className="go-to-btn"
+                    onClick={() => {
+                        console.log('click')
+                        inProgress()
+                        setStage(1)
+                    }}
+                >
+                    미닝템 정하러 가기
+                </button>
             </div>
         }
         {stage !== 0 &&
             <div className="flex justify-center mt-10">
-                <ChallengeInfo item={"닌텐도"} day={50} date={1} itemImg={"/images/nintendo.png"} />
+                <ChallengeInfo item={productName} day={challengePeriod} date={days} itemImg={productImg} />
             </div>
         }
         {stage === 5 &&
