4b8a3d88ff5943433523db63dbc90496960562e5
이승민 <0283esm@naver.com>
2023-09-20 13:29:59 +0900

    [S09P22A110-437] feat: Transfer.java BaseTimeEntity를 상속하도록 수정

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/domain/Transfer.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/domain/Transfer.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "transfer")
 @Builder
-public class Transfer {
+public class Transfer extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
