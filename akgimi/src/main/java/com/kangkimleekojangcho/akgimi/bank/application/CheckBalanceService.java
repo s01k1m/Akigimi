d0edcf75d1fbd57df762238b9112e75766eeb4b9
이승민 <0283esm@naver.com>
2023-09-20 13:21:19 +0900

    [S09P22A110-442] feat: 계좌 잔액 조회 구현 완료

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/application/CheckBalanceService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/application/CheckBalanceService.java
@@ -1,13 +1,26 @@
 package com.kangkimleekojangcho.akgimi.bank.application;
 
+import com.kangkimleekojangcho.akgimi.bank.application.port.QueryAccountDbPort;
 import com.kangkimleekojangcho.akgimi.bank.application.response.CheckBalanceServiceResponse;
+import com.kangkimleekojangcho.akgimi.bank.domain.Account;
+import com.kangkimleekojangcho.akgimi.bank.domain.AccountType;
+import com.kangkimleekojangcho.akgimi.global.exception.BadRequestException;
+import com.kangkimleekojangcho.akgimi.global.exception.BadRequestExceptionCode;
+import com.kangkimleekojangcho.akgimi.user.application.port.QueryUserDbPort;
+import com.kangkimleekojangcho.akgimi.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CheckBalanceService {
-    public CheckBalanceServiceResponse checkBalance(long userId, String accountType) {
-        return null;
+    private final QueryUserDbPort queryUserDbPort;
+    private final QueryAccountDbPort queryAccountDbPort;
+    public CheckBalanceServiceResponse checkBalance(long userId, AccountType accountType) {
+        User user = queryUserDbPort.findById(userId)
+                .orElseThrow(() -> new BadRequestException(BadRequestExceptionCode.NOT_USER));
+        Account account = queryAccountDbPort.findByUserAndAccountType(user,accountType)
+                .orElseThrow(() -> new BadRequestException(BadRequestExceptionCode.NO_BANK_ACCOUNT));
+        return new CheckBalanceServiceResponse(account.getBalance());
     }
 }
