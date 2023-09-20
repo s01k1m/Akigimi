979b76bc5752a24d5c1a5a224399ce86ce312fc5
이승민 <0283esm@naver.com>
2023-09-20 13:43:58 +0900

    [S09P22A110-442] feat: 계좌 잔액 조회 Transactional 추가
    Former-commit-id: 119b6105700cb05f731317ad6db3c0d7666f0fac

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/application/CheckBalanceService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/bank/application/CheckBalanceService.java
@@ -10,12 +10,15 @@ import com.kangkimleekojangcho.akgimi.user.application.port.QueryUserDbPort;
 import com.kangkimleekojangcho.akgimi.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CheckBalanceService {
     private final QueryUserDbPort queryUserDbPort;
     private final QueryAccountDbPort queryAccountDbPort;
+
+    @Transactional(readOnly = true)
     public CheckBalanceServiceResponse checkBalance(long userId, AccountType accountType) {
         User user = queryUserDbPort.findById(userId)
                 .orElseThrow(() -> new BadRequestException(BadRequestExceptionCode.NOT_USER));
