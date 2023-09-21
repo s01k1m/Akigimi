e02d5f23ca634739df5961228bfd54b1c4e2f72c
이승민 <0283esm@naver.com>
2023-09-21 16:26:22 +0900

    [S09P22A110-551] feat: Follow 로직 작성 완료
    Former-commit-id: 70d94103ad6e67a450df9fff497a1baf17dcc98a

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/domain/Follow.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/domain/Follow.java
@@ -2,6 +2,7 @@ package com.kangkimleekojangcho.akgimi.user.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -26,4 +27,12 @@ public class Follow{
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreatedDate
     private LocalDateTime followedAt;
+
+    @Builder
+    public Follow(Long id, User follower, User followee, LocalDateTime followedAt) {
+        this.id = id;
+        this.follower = follower;
+        this.followee = followee;
+        this.followedAt = followedAt;
+    }
 }
