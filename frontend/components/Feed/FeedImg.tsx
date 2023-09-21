821c333b28fb9b5336c7c63c7226593bbd28cca9
seoyoung81 <seosue0801@gmail.com>
2023-09-21 18:00:48 +0900

    feature: 무한스크롤 적용 중
    Former-commit-id: ee0754aaad5ce5c852b8031cffae713830b1c496

--- frontend/components/Feed/FeedImg.tsx
+++ frontend/components/Feed/FeedImg.tsx
@@ -7,13 +7,13 @@ interface FeedImgProps {
 const FeedImg: React.FC<FeedImgProps> = ({ image }) => {
     return (
         <div className="flex justify-center">
-            <Image
+            {/* <Image
                 src={image}
                 alt="유저가 올린 사진"
                 width={250}
                 height={150}
                 className="h-[20vh] mx-[5vw] overflow-hidden rounded-md"  
-            />
+            /> */}
         </div>
     )
 }
--- frontend/components/Feed/FeedList.tsx
+++ frontend/components/Feed/FeedList.tsx
@@ -1,6 +1,7 @@
 import FeedItem from "./FeedItem"
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { useInView } from "react-intersection-observer";
 
 type FeedItem = {
     id: number;
@@ -18,8 +19,19 @@ type selectedProps =  {
 }
 
 const FeedList: React.FC<selectedProps> = ({ selectedValue }) => {
-    const [visibleItems, setVisibleItems] = useState<number>(4);
-    const [feedItems, setFeedItems] = useState<FeedItem[]>();
+    // 초기 피드 리스트
+    const [feedItems, setFeedItems] = useState<FeedItem[]>([]);
+
+    // 새로 불러오는 리스트의 값만 담을 변수
+    let toCheckFeedId = []
+ 
+    // 관찰할 객체에 ref 달기
+    const [ref, inView] = useInView()
+
+    // 요청 보낼 마지막 lastViewId
+    const [lastViewId, setLastViewId] = useState<number>(9223372036854775807);
+
+    // 다음 피드가 불러오질 때까지 로딩
     const [loading, setLoading] = useState<boolean>(false);
 
     // selectedValue 선택 된 값 파라미터로 넘기기
@@ -27,12 +39,13 @@ const FeedList: React.FC<selectedProps> = ({ selectedValue }) => {
     const [feedData, setFeedData] = useState<[]>()
     const token = `eyJ0eXBlIjoiQUNDRVNTVE9LRU4iLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OTk5OSwidXNlclN0YXRlIjoiUEVORElORyIsImlhdCI6MTY5NTI1NTQ5MywiZXhwIjoxNjk1NDM1NDkzfQ.Wwg5ar8uOp2xZmt6JO7aRyhPTHuIxduFcrx1pdV-vAM`
 
-    // 피드 정보 api 호출하기
+    // 피드 정보 api 호출하기 // 무한 스크롤 적용하기
     const getFeedData = async () => {
+      setLoading(true)
         await axios
             .get('http://25.7.186.86:8080/api/feeds', {
                 params: {
-                  lastFeedId: 100,
+                  lastFeedId: 9,
                   numberOfFeed: 5
                 },
                 headers: {
@@ -41,21 +54,47 @@ const FeedList: React.FC<selectedProps> = ({ selectedValue }) => {
             })
             .then((response) => {
                 console.log('피드 조회 성공', response)
-                setFeedItems(response.data.data.list)
+
+                // 요청 성공 시에 리스트 뒤로 붙여주기
+                setFeedItems([...feedItems, ...(response.data.data.list)])
+
+                // 새로 가져온 값만 리스트에 담아주기
+                toCheckFeedId.push([response.data.data.list])
+
+                // 첫 번째 요청 이후에 viewId 값 상태 변경해주기
+                setLastViewId(toCheckFeedId[toCheckFeedId.length-1])
+                console.log('마지막 피드의 아이디 값은?', lastViewId)
+
+                
+              })
+            .then(() => {
+              // 마지막 요청을 판단하는 방법
+
             })
             .catch((error) => {
-                console.log('피드 조회 실패', error.response)
+                console.log('피드 조회 실패', error)
             })
+            .finally (() => {
+              setLoading(false)
+            }
+)
     }
     
     useEffect(() => {
-      getFeedData()
-    }, [])
+      // inView가 true 일 때만 실행한다
+      if (inView) {
+        console.log(inView, '무한 스크롤 요청 중이에요')
+
+        // 실행할 함수
+        getFeedData()
+      }
+    }, [inView])
 
     return (
         <div className="mx-0">
             {feedItems && feedItems.length > 0 &&
-            feedItems.slice(0, visibleItems).map((item: any) => (
+            feedItems.map((item: any) => (
+              <>
                 <FeedItem 
                     key={item.id} 
                     itemId={item.id}
@@ -67,7 +106,9 @@ const FeedList: React.FC<selectedProps> = ({ selectedValue }) => {
                     image={item.photo} 
                     isLiked={item.isLikedFeed} 
                     description={item.content} />
+              </>
             ))}
+            <div ref={ref}>스크롤을 내리고 있어요</div>
             {loading && <div>Loading...</div>}
         </div>
     )
