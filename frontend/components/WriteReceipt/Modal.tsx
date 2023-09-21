64ee2f6d5c036ed76e33c1e25e4787d538c04444
seoyoung81 <seosue0801@gmail.com>
2023-09-21 14:35:37 +0900

    feature: 잔액 부족한 경우 모달 띄우고, 계좌 잔액 API 연동하기

--- frontend/components/WriteReceipt/Modal.tsx
+++ frontend/components/WriteReceipt/Modal.tsx
@@ -1,19 +1,53 @@
-interface ModalProps {
-    change: number
-}
+'use client'
+import { useState, useEffect, useRef } from "react"
+import axios from "axios"
 
-const Modal: React.FC<ModalProps> = ({ change }) => {
+
+const Modal = () => {
+    // 계좌 잔액 상태 관리
+    const [balance, setBalance] = useState<number>()
+    const modalRef = useRef<HTMLDivElement>(null)
+
+    // 계좌 조회 API
+    const checkBalance = async () => {
+        const token = `eyJ0eXBlIjoiQUNDRVNTVE9LRU4iLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OTk5OSwidXNlclN0YXRlIjoiUEVORElORyIsImlhdCI6MTY5NTI1NTQ5MywiZXhwIjoxNjk1NDM1NDkzfQ.Wwg5ar8uOp2xZmt6JO7aRyhPTHuIxduFcrx1pdV-vAM`
+        
+        await axios
+        .get('http://25.7.186.86:8080/api/account/amount', {
+            params: {
+                accountType: 'DEPOSIT'
+            },
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+        .then((response) => {
+            setBalance(response.data.data.balance)
+            console.log('계좌 잔액 조회 성공', response.data)
+            
+        })
+        .catch((error) => {
+            console.log('계좌 잔액 조회 실패', error)
+        })
+    }
+
+    useEffect(() => {
+        // checkBalance()
+    }, [])
+    
     return (
-        <div className="">
-            <div className="h-[180px] bg-white rounded-2xl drop-shadow-lg p-8">
+        <div>
+            <div className="relative h-[250px] bg-white rounded-2xl drop-shadow-lg p-8">
+                <div className="absolute right-7 text-[20px]"
+                >X</div>
                 {/* use 정보는 redux에서 가져오자 */}
                 <p className="modal-big-text pb-2">{'잘생긴 김솔'}님,</p>
                 <div className="modal-big-text pb-2">출금계좌의 잔액을</div>
                 <div className="modal-big-text">확인해주세요.</div>
-                <p className="modal-small-text">현재 잔액은 {change}원이에요.</p>
+                <p className="modal-small-text mt-[3vh]">현재 잔액은 {balance}원이에요.</p>
             </div>
         </div>
-    )
-}
+    )}
+
 
 export default Modal
