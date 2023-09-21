f49dec99eecbc3a4cbaea2b9910b9c030566b3c5
KBS <qudtjs3636@gmail.com>
2023-09-21 12:38:32 +0900

    feat: testcode 작성을 위한 코드 리펙토링 및 test container 설정
    외부 환ê² 환경변수를 주입받ë¡도록 변경한다.
    test container를 사용해 redis test를 가능하게 한다
    
    Signed-off-by: KBS <qudtjs3636@gmail.com>

--- akgimi/src/test/java/com/kangkimleekojangcho/akgimi/ServiceIntegrationTestSupport.java
+++ akgimi/src/test/java/com/kangkimleekojangcho/akgimi/ServiceIntegrationTestSupport.java
@@ -1,12 +1,41 @@
 package com.kangkimleekojangcho.akgimi;
 
 
+import com.kangkimleekojangcho.akgimi.common.config.S3Config;
+import com.kangkimleekojangcho.akgimi.sns.application.port.CommandFeedImagePort;
+import com.kangkimleekojangcho.akgimi.user.config.KakaoProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@RequiredArgsConstructor
 public abstract class ServiceIntegrationTestSupport {
 
+    static {
+        List<String> portBindings = new ArrayList<>();
+        portBindings.add("6380:6379");
+
+        GenericContainer<?> redis =
+                new GenericContainer<>(DockerImageName.parse("redis:7.2.1-alpine"));
+        redis.setPortBindings(portBindings);
+        redis.start();
+    }
+
+    @MockBean
+    protected CommandFeedImagePort commandFeedImagePort;
+
+    @MockBean
+    protected S3Config s3Config;
+
+    @MockBean
+    protected KakaoProperties kakaoProperties;
 
 }
