a77df0d46fa95b9fcdcec1881790503742b22419
seoyoung81 <seosue0801@gmail.com>
2023-09-21 09:35:28 +0900

    feat: 게이지바 스테이지 -1 일 때, 아무것도 없게 경우 나누기
    Former-commit-id: d38d720070cb4f6130ecf1aa96dfb9d695cc4c92

--- frontend/components/Main/DayGageBar.tsx
+++ frontend/components/Main/DayGageBar.tsx
@@ -7,9 +7,10 @@ import Image from 'next/image';
 interface DayPropsType {
     days: number
     challengePeriod: number
+    stage: number
 }
 
-const DayGageBar: React.FC<DayPropsType> = ({ days, challengePeriod }) => {
+const DayGageBar: React.FC<DayPropsType> = ({ days, challengePeriod, stage }) => {
 
     // gage 받아서 너비 계산 해줘야됨
     const [gage, setGage] = useState<number>(100)
@@ -17,18 +18,25 @@ const DayGageBar: React.FC<DayPropsType> = ({ days, challengePeriod }) => {
         <>
         <div className='Gage-Bar relative flex items-center justify-center'>
             <p className='flex z-10'>
-                {1}/{50}
+                {days}/{challengePeriod}
             </p>
-            <div className='Date-Gage z-0 absolute top-1/2 left-0 transform -translate-y-1/2 min-w-[10%]'  style={{ width: `${(1/50)*100}%` }}>
+            {(stage === 0) ? (
+                <div className='Date-Gage z-0 absolute top-1/2 left-0 transform -translate-y-1/2' >
                 <div className="flex items-center translate-y-2">
-                    <Image 
-                        src="/images/runner.png" 
-                        alt="gift" 
-                        width={60} 
-                        height={60} 
-                        className="z-0 absolute right-[-25px] min-w-[50px]"/>
                 </div>
-            </div>
+            </div> 
+            ) : (
+                <div className='Date-Gage z-0 absolute top-1/2 left-0 transform -translate-y-1/2 min-w-[10%]'  style={{ width: `${(days/challengePeriod)*100}%` }}>
+                    <div className="flex items-center translate-y-2">
+                        <Image 
+                            src="/images/runner.png" 
+                            alt="gift" 
+                            width={60} 
+                            height={60} 
+                            className="z-0 absolute right-[-25px] min-w-[50px]"/>
+                    </div>
+                </div>   
+            )}
         </div>
         </>
     )
