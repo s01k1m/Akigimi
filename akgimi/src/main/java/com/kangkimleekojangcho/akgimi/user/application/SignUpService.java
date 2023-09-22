ca7ec214a7eb708e354aaff0b992f7b6f3d64a3d
seongchan cho <wordnumperiod@gmail.com>
2023-09-22 15:46:54 +0900

    feat: 중복 로그인 방지 코드 개발
    Former-commit-id: 9224aa860213ab39123b0852e6dae63f706c6923

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/application/SignUpService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/application/SignUpService.java
@@ -1,11 +1,16 @@
 package com.kangkimleekojangcho.akgimi.user.application;
 
+import com.kangkimleekojangcho.akgimi.global.exception.BadRequestException;
+import com.kangkimleekojangcho.akgimi.global.exception.BadRequestExceptionCode;
 import com.kangkimleekojangcho.akgimi.user.application.port.CommandUserDbPort;
+import com.kangkimleekojangcho.akgimi.user.application.port.QueryUserDbPort;
 import com.kangkimleekojangcho.akgimi.user.application.response.SignUpServiceResponse;
 import com.kangkimleekojangcho.akgimi.user.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,10 +18,15 @@ public class SignUpService {
     private final CommandUserDbPort commandUserDbPort;
     private final DecodeIdTokenService decodeIdTokenService;
     private final JwtTokenIssuer jwtTokenIssuer;
+    private final QueryUserDbPort queryUserDbPort;
     @Transactional
     public SignUpServiceResponse signUp(String rawIdToken) {
         KakaoIdToken idToken = decodeIdTokenService.decode(rawIdToken);
         String oauthId = idToken.getSub();
+        Optional<User> userOpt = queryUserDbPort.findByOauthId(oauthId);
+        if (userOpt.isPresent()) {
+            throw new BadRequestException(BadRequestExceptionCode.ALREADY_USER);
+        }
         User user = User.builder()
                 .oauthId(oauthId)
                 .oidcProvider(OidcProvider.KAKAO)
