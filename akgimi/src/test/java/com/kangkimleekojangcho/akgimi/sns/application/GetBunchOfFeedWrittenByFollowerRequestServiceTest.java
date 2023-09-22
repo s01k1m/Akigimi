3e742ad49272576ed3e5caea1a0114facdf7da2c
KBS <qudtjs3636@gmail.com>
2023-09-22 13:00:19 +0900

    test: receipt 및 feed test 생성. 현재 feed 수정 필요
    Signed-off-by: KBS <qudtjs3636@gmail.com>

--- akgimi/src/test/java/com/kangkimleekojangcho/akgimi/sns/application/GetBunchOfFeedWrittenByFollowerRequestServiceTest.java
+++ akgimi/src/test/java/com/kangkimleekojangcho/akgimi/sns/application/GetBunchOfFeedWrittenByFollowerRequestServiceTest.java
@@ -5,8 +5,8 @@ import com.kangkimleekojangcho.akgimi.bank.domain.AccountType;
 import com.kangkimleekojangcho.akgimi.bank.domain.Bank;
 import com.kangkimleekojangcho.akgimi.challenge.domain.Challenge;
 import com.kangkimleekojangcho.akgimi.product.domain.Product;
-import com.kangkimleekojangcho.akgimi.sns.application.request.GetBunchOfReceiptServiceRequest;
-import com.kangkimleekojangcho.akgimi.sns.application.response.GetBunchOfReceiptServiceResponse;
+import com.kangkimleekojangcho.akgimi.sns.application.request.GetBunchOfFeedWrittenByFollowerRequestServiceRequest;
+import com.kangkimleekojangcho.akgimi.sns.application.response.GetBunchOfFeedWrittenByFollowerServiceResponse;
 import com.kangkimleekojangcho.akgimi.sns.domain.Feed;
 import com.kangkimleekojangcho.akgimi.user.domain.KakaoNickname;
 import com.kangkimleekojangcho.akgimi.user.domain.User;
@@ -16,7 +16,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,13 +23,13 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Transactional
-class GetBunchOfReceiptServiceTest extends SnsServiceIntegrationTestSupport {
+class GetBunchOfFeedWrittenByFollowerRequestServiceTest extends SnsServiceIntegrationTestSupport {
 
     @Autowired
-    private GetBunchOfReceiptService getBunchOfReceiptService;
+    private GetBunchOfFeedWrittenByFollowerRequestService getBunchOfFeedWrittenByFollowerRequestService;
 
     private static final int NUMBER_OF_PUBLIC = 10;
+
     @DisplayName("[happy]본인의 영수증을 요청했을 때 정확한 데이터가 주어지면 요청에 따라 영수증 리스트를 반환한다.")
     @MethodSource("givenSameUserAnsAnswer")
     @ParameterizedTest
@@ -40,59 +39,30 @@ class GetBunchOfReceiptServiceTest extends SnsServiceIntegrationTestSupport {
             int answer
     ) throws Exception {
         //given
-        GetBunchOfReceiptServiceRequest getBunchOfReceiptServiceRequest =
-                GetBunchOfReceiptServiceRequest.builder()
-                        .lastReceiptId(Long.MAX_VALUE)
-                        .numberOfReceipt(size)
+        GetBunchOfFeedWrittenByFollowerRequestServiceRequest getBunchOfFeedServiceRequest =
+                GetBunchOfFeedWrittenByFollowerRequestServiceRequest.builder()
+                        .lastFeedId(Long.MAX_VALUE)
+                        .numberOfFeed(size)
                         .build();
         requestUser = commandUserDbPort.save(requestUser);
         prepareForSelectReceipt(requestUser);
 
         //when
-        GetBunchOfReceiptServiceResponse getBunchOfReceiptServiceResponse
-                = getBunchOfReceiptService.getBunchOfReceipt(
-                requestUser.getId(), requestUser.getId(), getBunchOfReceiptServiceRequest);
+        GetBunchOfFeedWrittenByFollowerServiceResponse getBunchOfReceiptServiceResponse
+                = getBunchOfFeedWrittenByFollowerRequestService.getBunchOfFeed(
+                requestUser.getId(), getBunchOfFeedServiceRequest);
 
         //then
-        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefReceiptInfo()).isNotNull();
-        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefReceiptInfo().size()).isEqualTo(answer);
+        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefFeedInfo()).isNotNull();
+        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefFeedInfo().size()).isEqualTo(answer);
     }
-
-    @DisplayName("[happy]본인의 영수증이 아닌 다른 유저의 영수증을 요청했을 때 정확한 데이터가 주어지면 요청에 따라 영수증 리스트를 반환한다.")
-    @MethodSource("giveDifferentUserAndAnswer")
-    @ParameterizedTest
-    void givenValidData_whenDifferentUserRequestGetBunchOfReceipt_thenReturnData(
-            User requestUser,
-            User receiptOwner,
-            int size,
-            int answer
-    ) throws Exception {
-        //given
-        GetBunchOfReceiptServiceRequest getBunchOfReceiptServiceRequest =
-                GetBunchOfReceiptServiceRequest.builder()
-                        .lastReceiptId(Long.MAX_VALUE)
-                        .numberOfReceipt(size)
-                        .build();
-        requestUser = commandUserDbPort.save(requestUser);
-        receiptOwner = commandUserDbPort.save(receiptOwner);
-        prepareForSelectReceipt(requestUser);
-
-        //when
-        GetBunchOfReceiptServiceResponse getBunchOfReceiptServiceResponse
-                = getBunchOfReceiptService.getBunchOfReceipt(
-                requestUser.getId(), receiptOwner.getId(), getBunchOfReceiptServiceRequest);
-
-        //then
-        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefReceiptInfo()).isNotNull();
-        assertThat(getBunchOfReceiptServiceResponse.bunchOfBriefReceiptInfo().size()).isEqualTo(answer);
-    }
-
 
     private static Stream<Arguments> givenSameUserAnsAnswer() {
         User user = User.builder()
                 .kakaoProfileNickname(new KakaoNickname("카카오프로필"))
+                .profileImageUrl("httt")
+                .nickname("helllloooo")
                 .userState(UserState.ACTIVE)
-                .nickname("돌아다니는 카카오")
                 .oauthId("abcde")
                 .build();
         return Stream.of(
@@ -106,35 +76,6 @@ class GetBunchOfReceiptServiceTest extends SnsServiceIntegrationTestSupport {
                         user, 10, 10
                 )
         );
-    }
-
-    //TODO: bad case추가해야 함. 아직 어떻게 bad case를 설정해줘야 할지 모르겠음. 없는 유저가 보내는 요청도 처리해야 되니까.. 다만들어?
-
-    private static Stream<Arguments> giveDifferentUserAndAnswer() {
-        User user = User.builder()
-                .kakaoProfileNickname(new KakaoNickname("카카오프로필"))
-                .userState(UserState.ACTIVE)
-                .nickname("돌아다니는 카카오")
-                .oauthId("abcde")
-                .build();
-        User user2 = User.builder()
-                .kakaoProfileNickname(new KakaoNickname("카카오프로필"))
-                .userState(UserState.ACTIVE)
-                .nickname("돌아다니는 카카오2")
-                .oauthId("abcdef")
-                .build();
-        return Stream.of(
-                Arguments.of(
-                        user, user2, 100, 10
-                ),
-                Arguments.of(
-                        user, user2, 0, 0
-                ),
-                Arguments.of(
-                        user, user2, 5, 5
-                )
-        );
-
     }
 
     private void prepareForSelectReceipt(User user) {
@@ -226,6 +167,4 @@ class GetBunchOfReceiptServiceTest extends SnsServiceIntegrationTestSupport {
                             .build());
         }
     }
-
-
 }
