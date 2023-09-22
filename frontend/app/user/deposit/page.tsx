c614a2832e6fd5911393b48c732c1e916ce8fecc
seoyoung81 <seosue0801@gmail.com>
2023-09-22 10:15:28 +0900

    feature: 페이지별로 footer 추가
    Former-commit-id: 63ad448d5688c0542a2c5ecca8bdea4ccbbaeaee

--- frontend/app/user/deposit/page.tsx
+++ frontend/app/user/deposit/page.tsx
@@ -1,11 +1,13 @@
 import AccountInfo from "@/components/TransactionHistory/AccountInfo"
 import History from "@/components/TransactionHistory/History"
+import Footer from "@/app/Footer"
 
 const TransactionHistory = () => {
     return (
         <div>
             <AccountInfo />
             <History />
+            <Footer />
         </div>
     )
 }
