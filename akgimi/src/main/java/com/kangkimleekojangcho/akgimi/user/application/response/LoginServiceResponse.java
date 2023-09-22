e58bbc4bc25ce0c216f7bc0cf1c4a7c19e452147
seongchan cho <wordnumperiod@gmail.com>
2023-09-22 10:14:46 +0900

    feat: 로그인 시 PENDING 상태인지 알려주는 플래그 값을 Rsponse로 받게 설정
    Former-commit-id: 8fd88938f9103f88ab390f7ada6266570c875eb0

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/application/response/LoginServiceResponse.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/user/application/response/LoginServiceResponse.java
@@ -1,14 +1,17 @@
 package com.kangkimleekojangcho.akgimi.user.application.response;
 
+import com.kangkimleekojangcho.akgimi.user.domain.UserState;
 import lombok.Getter;
 
 @Getter
 public class LoginServiceResponse {
     private final String accessToken;
     private final String refreshToken;
+    private final String userState;
 
-    public LoginServiceResponse(String accessToken, String refreshToken) {
+    public LoginServiceResponse(String accessToken, String refreshToken, UserState userState) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.userState = userState.name();
     }
 }
