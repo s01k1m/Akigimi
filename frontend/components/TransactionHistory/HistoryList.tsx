9b70416f01d00284fd245ba50ac206765a40a955
seoyoung81 <seosue0801@gmail.com>
2023-09-22 14:18:36 +0900

    feature: 계좌 잔액 조회 API 연동

--- frontend/components/TransactionHistory/HistoryList.tsx
+++ frontend/components/TransactionHistory/HistoryList.tsx
@@ -1,23 +1,58 @@
 import HistoryItem from "./HistoryItem"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 interface ListPropsType {
     period: string
     type: string
 }
 
+interface HistoryItem {
+    transferDateTime: string
+    type: string
+    amount: number
+    balance: number
+}
+
 const HistoryList: React.FC<ListPropsType> = ({ period, type }) => {
+    const [historyData, setHistoryData] = useState<HistoryItem[]>([]);
     // period, type 고려해서 api 무한 스크롤 불러오기
-    const HistoryData = [
-        {id: 1, transferDateTime: '8.7', type: '송금', amount: 5000, balance: 15000 },
-        {id: 2, transferDateTime: '8.6', type: '입금', amount: 5000, balance: 20000 },
-        {id: 3, transferDateTime: '8.5', type: '송금', amount: 5000, balance: 15000 },
-        {id: 4, transferDateTime: '8.5', type: '입금', amount: 5000, balance: 20000 }
-    ]
+
+    let token: string = "";
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+        token = window.localStorage.getItem("access_token");
+        }
+        getHistoryInfo()
+    }, []);
+
+    const getHistoryInfo = async () => {
+        await axios 
+            .get(`/api/account/transaction/history`, {
+                params: {
+                    accountType: 'WITHDRAW'
+                },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
+            })
+            .then((response) => {
+                console.log('거래 내역 조회 성공', response.data.data.transfer)
+                setHistoryData(response.data.data.transfer)
+       
+            })
+            .catch((error) => {
+                console.log('거래 내역 조회 실패', error)
+            })
+    }
+
+
     return (
         <div>
-            {HistoryData.map((item) => (
+            {historyData.map((item, index) => (
                 <HistoryItem
-                    key={item.id}
+                    key={index}
                     date={item.transferDateTime}
                     type={item.type}
                     amount={item.amount}
