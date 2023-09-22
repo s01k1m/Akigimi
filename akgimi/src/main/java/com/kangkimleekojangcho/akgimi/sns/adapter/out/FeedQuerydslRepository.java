2f2d9a4cf1f84fdf18e6868f731d2c025bc51e3a
KBS <qudtjs3636@gmail.com>
2023-09-22 18:08:20 +0900

    feat: 테스트 코드 짜던 것
    Signed-off-by: KBS <qudtjs3636@gmail.com>

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/sns/adapter/out/FeedQuerydslRepository.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/sns/adapter/out/FeedQuerydslRepository.java
@@ -5,6 +5,8 @@ import com.kangkimleekojangcho.akgimi.sns.application.response.BriefReceiptInfo;
 import com.kangkimleekojangcho.akgimi.sns.application.response.QBriefFeedInfo;
 import com.kangkimleekojangcho.akgimi.sns.application.response.QBriefReceiptInfo;
 import com.kangkimleekojangcho.akgimi.sns.domain.QFeed;
+import com.kangkimleekojangcho.akgimi.user.domain.QFollow;
+import com.kangkimleekojangcho.akgimi.user.domain.QUser;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +15,15 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Objects;
 
+import static com.kangkimleekojangcho.akgimi.sns.domain.QFeed.*;
+import static com.kangkimleekojangcho.akgimi.user.domain.QFollow.follow;
+import static com.kangkimleekojangcho.akgimi.user.domain.QUser.user;
+
 @Repository
 @RequiredArgsConstructor
 public class FeedQuerydslRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    private final QFeed feed = QFeed.feed;
-
 
     public List<BriefFeedInfo> findByUser_IdAndLastFeedIdAndNumberOfFeed(
             Long userId,
@@ -43,10 +47,21 @@ public class FeedQuerydslRepository {
                 )
                 .from(feed)
                 //TODO: 팔로우 기능 만들어지면 팔로우된 유저만 처리하도록 해주기.
-                .where(ltFeedId(lastFeedId),
-                        feed.user.id.eq(userId))
+//                .join(follow.follower, user)
+                //묵시적 조인은 허용하지 않는다.
+                //Cross 조인에 의해 모든 행이 카타시안 곱으로 나타남.
+                //그러면 이걸 join으로 걸어줘야 하는데 어떻게 걸것인가
+                //1.
+                .where(ltFeedId(lastFeedId), //최신순으로부터 가져온다.
+                        feed.user.id.eq(userId)
+                        .or(feed.isPublic.eq(true))//조건 1. feed의 userId가 요청자와 동일해야 한다.
+                )
                 .orderBy(feed.feedId.desc())
                 .limit(numberOfFeed).fetch();
+
+                //조건 2. feed의 userId가 팔로워와 동일해야 한다. + 팔로워의 사이즈와 동일해야 한다.
+        //      select * from follow where follower=userId
+                //조건 3. 활성화된 유저의 것만 보여주어야 한다.
         return result;
     }
 
--- akgimi/src/test/java/com/kangkimleekojangcho/akgimi/config/ServiceIntegrationTestSupport.java
+++ akgimi/src/test/java/com/kangkimleekojangcho/akgimi/config/ServiceIntegrationTestSupport.java
@@ -3,6 +3,7 @@ package com.kangkimleekojangcho.akgimi.config;
 
 import com.kangkimleekojangcho.akgimi.common.config.S3Config;
 import com.kangkimleekojangcho.akgimi.sns.application.port.CommandFeedImagePort;
+import com.kangkimleekojangcho.akgimi.user.application.port.CommandUserProfileImagePort;
 import com.kangkimleekojangcho.akgimi.user.config.KakaoProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,9 @@ public abstract class ServiceIntegrationTestSupport {
 
     @MockBean
     protected CommandFeedImagePort commandFeedImagePort;
+
+    @MockBean
+    protected CommandUserProfileImagePort commandUserProfileImagePort;
 
     @MockBean
     protected S3Config s3Config;
