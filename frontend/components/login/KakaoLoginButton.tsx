fc78888f0352a793f2a27a37cf7ea2c0cd616638
s01k1m <sk618dev@gmail.com>
2023-09-22 09:47:24 +0900

    fix: 카카오로고 사진 URL 정정
    위치 잘못 잡고 있어서 다시 URL 정정함

--- frontend/components/login/KakaoLoginButton.tsx
+++ frontend/components/login/KakaoLoginButton.tsx
@@ -61,7 +61,7 @@ const KakaoLoginButton = () => {
               {/* <Button.KakaoButton onClick={kakaoLogin}> */}
               <Button.KakaoButton>
                 <Image
-                  src="/kakao logo.png"
+                  src="/images/kakao logo.png"
                   alt="카카오 로고 사진"
                   width="20"
                   height="20"
