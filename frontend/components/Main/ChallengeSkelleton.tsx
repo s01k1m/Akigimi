e466d458866ee9c9870d1365014d0cb434b79f75
seoyoung81 <seosue0801@gmail.com>
2023-09-25 14:07:19 +0900

    feature: footer 수정 및 스켈레톤 UI

---
+++ frontend/components/Main/ChallengeSkelleton.tsx
@@ -0,0 +1,28 @@
+import MoneyGageBar from "./MoneyGageBar"
+import DayGageBar from "./DayGageBar"
+import IconBtn from "./IconBtn"
+import CharacterImg from "./CharacterImg"
+import Footer from "@/app/Footer"
+
+const ChallengeSkelleton = () => {
+    return (
+        <div>
+            <div style={{ width: '100%'}}>
+            <div className="flex flex-col items-center justify-center" style={{ width: '100%'}}>
+                <MoneyGageBar percentage={10} productPrice={0} balance={0} stage={1}  />
+                <DayGageBar challengePeriod={0} days={0} stage={1}   />
+            </div>
+            <div className="flex">
+                <IconBtn />
+            </div>
+            
+            <div className="flex justify-center w-[100%]">
+                <Footer />
+            </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ChallengeSkelleton
