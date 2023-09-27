fe69678c3f9944949a0df4ce87639c1424fdbfb2
KBS <qudtjs3636@gmail.com>
2023-09-27 11:28:46 +0900

    refactor: id 정보 추가
    Signed-off-by: KBS <qudtjs3636@gmail.com>
    
    Former-commit-id: 58af819ebf358abd43a7a3618c6d67c61902fc78

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/sns/application/response/BriefReceiptInfo.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/sns/application/response/BriefReceiptInfo.java
@@ -5,6 +5,7 @@ import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDateTime;
 
 public record BriefReceiptInfo(
+        Long receiptId,
         Long price,
         String notPurchasedItem,
         String akgimiPlace,
