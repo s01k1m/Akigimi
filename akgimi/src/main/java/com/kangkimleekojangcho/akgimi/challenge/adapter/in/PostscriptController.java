6946c856079d218dc5a22e7bc7f7e77f4ac78a3a
KBS <qudtjs0753@naver.com>
2023-09-27 16:59:14 +0900

    test: Postscript 생성 테스트코드 작성
    Signed-off-by: KBS <qudtjs0753@naver.com>

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/adapter/in/PostscriptController.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/adapter/in/PostscriptController.java
@@ -1,15 +1,25 @@
 package com.kangkimleekojangcho.akgimi.challenge.adapter.in;
 
+import com.kangkimleekojangcho.akgimi.challenge.adapter.in.request.CreatePostscriptRequest;
+import com.kangkimleekojangcho.akgimi.common.domain.application.SubtractUserIdFromAccessTokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/postscripts")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/postscripts")
 public class PostscriptController {
 
+    private final SubtractUserIdFromAccessTokenService userIdFromAccessTokenService;
+
     @PostMapping
-    public void createPostscript() {
+    public void createPostscript(@Valid CreatePostscriptRequest createPostscriptRequest, HttpServletRequest servletRequest) {
+        Long userId = userIdFromAccessTokenService.subtract(servletRequest);
 
     }
 }
