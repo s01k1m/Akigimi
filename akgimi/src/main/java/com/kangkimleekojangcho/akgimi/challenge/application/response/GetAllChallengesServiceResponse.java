c6681476c7f5ae2125bfafcb8e7309a3d583f384
zogak7 <zogak7@gmail.com>
2023-09-27 14:48:42 +0900

    [S09P22A110-657] update: 내가 작성한 모든 챌린지 조회 response에 며칠만에 성공했는지 일 수 추가
    Former-commit-id: 3302b6d7b2822c88073ecc68a77e0c8509f04ed2

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/response/GetAllChallengesServiceResponse.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/response/GetAllChallengesServiceResponse.java
@@ -17,9 +17,17 @@ public class GetAllChallengesServiceResponse {
     private final Integer challengePeriod;
     private final LocalDate challengeStartDate;
     private final LocalDate challengeEndDate;
+    private final Integer challengeDays;
     private final boolean isInProgress;
+    private final boolean isPostscriptCreated;
 
     public static GetAllChallengesServiceResponse from(Challenge challenge){
+        int days;
+        if(challenge.isInProgress()){
+            days = challenge.calculateDays(LocalDate.now());
+        }else{
+            days = challenge.calculateDays(challenge.getAchievementDate());
+        }
         return GetAllChallengesServiceResponse.builder()
                 .challengeId(challenge.getId())
                 .productId(challenge.getProduct().getId())
@@ -27,7 +35,9 @@ public class GetAllChallengesServiceResponse {
                 .challengePeriod(challenge.getChallengePeriod())
                 .challengeStartDate(challenge.getChallengeStartDate())
                 .challengeEndDate(challenge.getChallengeEndDate())
+                .challengeDays(days)
                 .isInProgress(challenge.isInProgress())
+                //TODO: .isPostscriptCreated()
                 .build();
     }
 }
