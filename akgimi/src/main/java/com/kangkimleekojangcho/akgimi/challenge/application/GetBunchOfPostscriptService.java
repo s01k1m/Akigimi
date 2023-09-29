d5013785798c63f13569247c9b38ab1beaf60a51
KBS <qudtjs0753@naver.com>
2023-09-29 14:47:24 +0900

    fix: 서비스 로직 수정
    Signed-off-by: KBS <qudtjs0753@naver.com>

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/GetBunchOfPostscriptService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/GetBunchOfPostscriptService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -15,6 +17,12 @@ public class GetBunchOfPostscriptService {
     private final QueryPostscriptDbPort queryPostscriptDbPort;
 
     public GetBunchOfPostscriptServiceResponse execute(GetBunchOfPostscriptServiceRequest request) {
+        if (request.numberOfPostscript() == 0) {
+            return GetBunchOfPostscriptServiceResponse.builder()
+                    .bunchOfPostscriptInfo(new ArrayList<>())
+                    .build();
+        }
+
         return queryPostscriptDbPort.findBunchOfPostscriptForProduct(request);
     }
 }
