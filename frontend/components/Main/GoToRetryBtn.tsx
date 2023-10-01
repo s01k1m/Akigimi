8b6239d47af5c09ace72ca60b0ba03335ed6fcb4
seoyoung81 <seosue0801@gmail.com>
2023-10-01 13:38:57 +0900

    add: react modal npm 설치

--- frontend/components/Main/GoToRetryBtn.tsx
+++ frontend/components/Main/GoToRetryBtn.tsx
@@ -1,15 +1,16 @@
-import { useRouter } from "next/navigation"
+import { useState, useEffect } from "react"
 
 const GoToRetryBtn = () => {
-    const router = useRouter()
+    const [isOpened, setIsOpened] = useState<boolean>(false);
+
     return (
         <div>
             <button 
                 className="button-common go-to-retry"
-                onClick={() => router.push('/retry')}
+                onClick={() => setIsOpened(true)}
             >다시 도전하기</button>
         </div>
     )
 }
 
-export default GoToRetryBtn
+export default GoToRetryBtn     
