4d2f10569332d0c4a9c568c75447c3376c431bc6
seoyoung81 <seosue0801@gmail.com>
2023-10-03 00:11:40 +0900

    feature: 좋아요, 좋아요 취소 api

---
+++ frontend/app/review/write/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/app/Footer"
+
+const reviewWrite = () => {
+    return (
+        <div>
+            리뷰 작성 페이지
+            <Footer />
+        </div>
+    )
+}
+
+export default reviewWrite
