604fb004758c59cee7068ba162ff21d7d42b5e93
seoyoung81 <seosue0801@gmail.com>
2023-10-03 14:18:03 +0900

    feature: 계좌별 거래내역 조회 구현
    Former-commit-id: 35a876919b077e7a85819d5b7c10fe7b0010150b

--- frontend/app/user/withdraw/page.tsx
+++ frontend/app/user/withdraw/page.tsx
@@ -2,11 +2,16 @@ import AccountInfo from "@/components/TransactionHistory/AccountInfo"
 import History from "@/components/TransactionHistory/History"
 import Footer from "@/app/Footer"
 
+interface typeProps {
+    type: string
+    accountType: string
+}
+
 const TransactionHistory = () => {
     return (
         <div>
-            <AccountInfo />
-            <History />
+            <AccountInfo type={"WITHDRAW"} />
+            <History accountType={"WITHDRAW"} />
             <Footer />
         </div>
     )
--- frontend/components/TransactionHistory/HistoryItem.tsx
+++ frontend/components/TransactionHistory/HistoryItem.tsx
@@ -19,10 +19,6 @@ const HistoryItem: React.FC<HistoryItemProps> = ({ date, type, amount, balance }
 
     const formattedDate = `${year}. ${month}. ${day}`;
 
-    
-    useEffect(() => {
-        console.log('지금 시간은', formattedDate)
-    }, [])
 
     return (
     <div className="flex justify-center">
@@ -32,14 +28,14 @@ const HistoryItem: React.FC<HistoryItemProps> = ({ date, type, amount, balance }
         <div className="flex w-[80%] justify-between mt-[3vh] mb-[5vh] items-center">
             <div className="text-sm font-semibold tracking-widest">{formattedDate}</div>
             <div className="flex flex-col justify-end">
-                {type==='송금' ? (
+                {type==='출금' ? (
                     <>
-                        <div className={`text-[#0049F2] text-[16px] tracking-widest`}>-{amount}원</div>
+                        <div className={`text-[16px] tracking-widest`}>-{amount}원</div>
                         <div className="text-[12px] font-light tracking-widest justify-end flex">{balance}원</div>
                     </>
                 ): (
                     <>
-                        <div className={`text-[16px] tracking-widest`}>{amount}원</div>
+                        <div className={`text-[16px] tracking-widest text-[#0049F2] `}>{amount}원</div>
                         <div className="text-[12px] font-light tracking-widest justify-end flex">{balance}원</div>
                     </>
                     
