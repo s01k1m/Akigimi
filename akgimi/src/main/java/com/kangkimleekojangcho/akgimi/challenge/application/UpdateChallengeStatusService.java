7ccda15a1860826735e243e4ea80eb3de18c33aa
zogak7 <zogak7@gmail.com>
2023-10-04 13:01:08 +0900

    [S09P22A110-659] update: Transactional 추가

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/UpdateChallengeStatusService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/challenge/application/UpdateChallengeStatusService.java
@@ -5,6 +5,7 @@ import com.kangkimleekojangcho.akgimi.challenge.application.response.UpdateChall
 import com.kangkimleekojangcho.akgimi.challenge.domain.Challenge;
 import com.kangkimleekojangcho.akgimi.global.exception.BadRequestException;
 import com.kangkimleekojangcho.akgimi.global.exception.BadRequestExceptionCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,12 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class UpdateChallengeStatusService {
     private final QueryChallengeDbPort queryChallengeDbPort;
-    private final CommandChallengeDbPort
-    commandChallengeDbPort;
 
+    @Transactional
     public UpdateChallengeStatusServiceResponse update(Long userId, boolean succeed){
         Challenge challenge = queryChallengeDbPort.findInProgressChallengeByUserId(userId).orElseThrow(()-> new BadRequestException(BadRequestExceptionCode.NOT_PARTICIPATE_IN_CHALLENGE));
         if(succeed){
             challenge.update(true, LocalDate.now(), false);
-            commandChallengeDbPort.save(challenge);
         }
         return  UpdateChallengeStatusServiceResponse.builder()
                 .productUrl(challenge.getProduct().getUrl())
