d8423b908aac1104562a10a0a902bf51ae04e39b
이승민 <0283esm@naver.com>
2023-10-04 12:39:00 +0900

    fix: Transactional readOnly 추가

--- akgimi/src/main/java/com/kangkimleekojangcho/akgimi/product/application/DetailProductService.java
+++ akgimi/src/main/java/com/kangkimleekojangcho/akgimi/product/application/DetailProductService.java
@@ -8,13 +8,14 @@ import com.kangkimleekojangcho.akgimi.product.application.response.ProductDetail
 import com.kangkimleekojangcho.akgimi.product.domain.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @RequiredArgsConstructor
 public class DetailProductService {
     private final QueryProductDbPort queryProductDbPort;
+    @Transactional(readOnly = true)
     public ProductDetailServiceResponse detail(ProductDetailServiceRequest serviceRequest) {
         Product product = queryProductDbPort.findById(serviceRequest.getId())
                 .orElseThrow(() -> new BadRequestException(BadRequestExceptionCode.NO_PRODUCT));
