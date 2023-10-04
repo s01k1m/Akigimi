bff75f550b3633e87206ce0cbb19c32d1db6b03f
seoyoung81 <seosue0801@gmail.com>
2023-10-04 09:51:37 +0900

    fix: git 대소문자 감지하도록 설정
    Former-commit-id: 5ca43c7a910cff90807db6a19b86158c44549d7c

---
+++ frontend/components/Login/RegisterHeader.tsx
@@ -0,0 +1,16 @@
+// import { RegisterInfo } from "@/app/login/register/withdraw/page";
+
+// type InfoProps = {
+//   info: RegisterInfo;
+// };
+
+// function Header({ info }) {
+//   const { title, menual } = info;
+
+//   return (
+//     <div>
+//       <div className="header text-[22px] pt-2 pb-[14px] font-bold">{title}</div>
+//       <div className="explanation text-base mb-[65px] text-grey0">{menual}</div>
+//     </div>
+//   );
+// }
