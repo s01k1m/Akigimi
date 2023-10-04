8062b81f5b494c10dd5ca01f89b8f522ef5cf8f9
seoyoung81 <seosue0801@gmail.com>
2023-10-04 13:35:38 +0900

    feat: 피드 사진 이미지대로 자르기
    Former-commit-id: 01258afba15746015392a10ed5b527ecb4c7bc1c

--- frontend/components/Feed/FeedImg.tsx
+++ frontend/components/Feed/FeedImg.tsx
@@ -6,15 +6,18 @@ interface FeedImgProps {
 
 const FeedImg: React.FC<FeedImgProps> = ({ image }) => {
     return (
-        <div className="flex justify-center">
+        <>
+        <div className="absolute flex justify-center rounded-md" style={{ width: '280px', height: '150px', overflow: 'hidden' }}>
             <Image
                 src={image}
                 alt="유저가 올린 사진"
-                width={250}
-                height={150}
+                layout="fill"
+                objectFit="cover"
                 className="h-[20vh] mx-[5vw] overflow-hidden rounded-md"  
             />
         </div>
+            <div className="h-[150px]"></div>
+        </>
     )
 }
 
