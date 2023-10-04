8c8c532a40b9c53f1d8338ad5238d5a9f4a913d7
seoyoung81 <seosue0801@gmail.com>
2023-10-05 01:24:13 +0900

    feat: 영수증 이미지 사이즈 자르기

--- frontend/components/MyReceipt/ReceiptItem.tsx
+++ frontend/components/MyReceipt/ReceiptItem.tsx
@@ -29,15 +29,15 @@ const ReceiptItem: React.FC<itemProps> = ({ akimPlace, saving, date, imgUrl }) =
             <div className="text-[20px] font-bold mb-1">{formattedDate}</div>
             <div className="flex flex-col mb-[2vh] bg-white w-[50vw] rounded-xl h-[100px] max-w-[280px] min-w-[200px]">
                 <div className="flex flex-end align-bottom ms-[5%] mt-[1vh]">
-                        <div style={{ width: '80px', height: '85px', overflow: 'hidden' }} className="me-2 flex">
+                    <div className="w-[100px]"></div>
+                        <div style={{ width: '80px', height: '80px', overflow: 'hidden' }} className="me-2 flex absolute rounded-lg">
                             <Image src={imgUrl} alt="영수증 사진" 
                                 className="me-[2vw]"
                                 objectFit="cover"
-                                width={80}
-                                height={200}
+                                layout="fill"
                                 />
                         </div>
-                        <div className="flex flex-col align-bottom justify-end">
+                        <div className="flex flex-col align-bottom justify-end mt-[30px]">
                             <div className="receipt-place">{akimPlace}</div>
                             <div>
                                 <span className="receipt-saving">{saving}</span>
